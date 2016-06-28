ad57a224ce5c4aebb5cdcb5d47f119079cbe3c27
andyphp <1010707582@qq.com>
2016-06-28 20:43:06 +0800

    提交登录模块

---
+++ application/api/controller/Common.php
@@ -0,0 +1,69 @@
+<?php
+namespace app\api\controller;
+
+/**
+ * 公共控制器
+ */
+class Common
+{
+
+	/**
+	 * [__construct description]
+	 */
+	public function __construct(){
+		//判断请求方式是否合法 
+		$this->checkMethod();
+		//判断用户是否有权限操作当前方法
+		$this->checkAuth();
+	}
+
+	/**
+	 * [getInput 获取请求参数]
+	 * 自定义方法
+	 * @return [type] [description]
+	 */
+	public function getInput(){
+		return json_decode(file_get_contents('php://input'),true);
+	}
+
+	/**
+	 * [checkMethod 使用函数助手 判断请求方式是否合法 默认所有请求都是 POST]
+	 * @return [type] [description]
+	 */
+	protected function checkMethod(){
+		if ( request()->method() != 'POST' ) {
+			return json(['status'=>0,'message'=>'请求方式错误!']);
+		}
+	}
+
+	/**
+	 * [checkAuth 检管理员的权限 add-1  deltet-2 edit-3 ]  
+	 * xuwenming
+	 * @return [type] [description]
+	 */
+	protected function checkAuth(){
+		$action = request()->action();
+		$auth = array(
+			'add' 		=> 1,
+			'delete' 	=> 2,
+			'edit' 		=> 3
+		);
+
+		$code = $auth[$action];
+
+		if (!in_array($code, $_SESSION['admin']['auth'])) {
+			return json(['status'=>0,'message'=>'没有操作权限!']);
+		}
+		
+	}
+
+	/**
+	 * [_empty]
+	 * @return [type] [description]
+	 */
+    public function _empty()
+    {
+        return json(['status'=>0,'message'=>'请求的方法不存在']);
+    }
+
+}
